a6b49fe2aa526c252b24c6a51f21ddf5cbd2bea6
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-13 22:48:07 -0700

    added some pseudo code to server.js
    needed to add some pseudo code to help set up what needs to be done next.

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const bodyParser = require("body-parser");
+const app = express();
+//const bodyParser = require("body-parser");
+
+// need to set port for listening
+const PORT = process.env.PORT || 4300;
+
+// need to initialize array that will hold data from notes
+let notesArray = [];
+
+// need to set up parsing, routing middleware, and static path for files
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+// set up response that sends results to browser in an array of objects
+
+    // try
+    // catch
+
+// set up section that writes new notes to JSON object
+
+    // try
+    // catch
+
+// Set up note deletion function
+
+    // try
+    // catch
+
+// HTML requests (GET) go here
+
+
+
