16123b7cfeb9f20bba6eb1f1539557d595ac9d5f
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-20 13:46:08 -0700

    Added code to schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,9 +2,22 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
 
-CREATE TABLE departements (
+/* Create table for departments */
+CREATE TABLE departments (
     id INTEGER,
     AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR (30),
 );
+
+/* Create table for roles */
+CREATE TABLE roles (
+    id INT,
+    AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(7,2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments (id),
+);
+
+/* Create tables for employees */
 
