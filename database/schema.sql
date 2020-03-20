f17780c5115d04f8b65e49efaa4222dd6ec3810c
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-20 10:50:20 -0700

    created schema and seed sql files

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+
+CREATE TABLE departements (
+    id INTEGER,
+    AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR (30),
+);
+
